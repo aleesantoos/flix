782f85f64693fe624079972fb98fd653bb097d7c
aleesantoos <69517158+aleesantoos@users.noreply.github.com>
2020-10-04 22:01:02 -0500

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Alejandra Santos on 10/4/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
